9a9fbb71e99ea9649d6a527f9101e4bf6077de9d
Amir Ebrahimi <amir@luminaryproductions.net>
2014-04-17 14:49:43 -0700

    Initial version

---
+++ Scripts/SetPropertyExample.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) 2014 Luminary LLC
+// Licensed under The MIT License (See LICENSE for full text)
+using UnityEngine;
+using System.Collections;
+
+public class SetPropertyExample : MonoBehaviour 
+{
+	[System.Serializable]
+	public class VanillaClass
+	{
+		public enum ExtraType
+		{
+			None,
+			HotFudge,
+			Mint,
+		}
+
+		[SerializeField]
+		[SetProperty("Extra")]
+		private ExtraType extra;
+		public ExtraType Extra
+		{
+			get
+			{
+				return extra;
+			}
+			set
+			{
+				extra = value;
+
+				// For illustrative purposes
+				if (value == ExtraType.None)
+				{
+					Debug.Log("Simple!");
+				}
+				else
+				{
+					Debug.Log ("Yummy!");
+				}
+			}
+		}
+	}
+
+	[SerializeField]
+	[SetProperty("Number")]
+	private float number;
+	public float Number
+	{
+		get
+		{
+			return number;
+		}
+		private set
+		{
+			number = Mathf.Clamp01(value);
+		}
+	}
+
+	[SerializeField]
+	[SetProperty("Text")]
+	private string text;
+	public string Text
+	{
+		get
+		{
+			return text;
+		}
+		set
+		{
+			text = value.ToUpper();
+		}
+	}
+
+	public VanillaClass vanilla;
+}
